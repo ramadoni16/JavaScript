1b944d4d4c7a3728315705a4379117dec282d9e4
ramadoni16 <donicyberit@gmail.com>
2024-07-21 22:47:26 +0700

    Contoh Perulangan For

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-for (let i = 1; i <= 10; i++) {
-  console.log("saya ada di dalam loop");
+// for (let i = 1; i <= 10; i++) {
+//   console.log("saya ada di dalam loop");
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 20; i += 2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 20; i >= 0; i -= 2) {
+//   console.log(i);
+// }
+
+for (let i = 10; i <= 100000; i *= 10) {
   console.log(i);
 }
