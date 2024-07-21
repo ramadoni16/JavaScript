6618a4a9800f0cfbbddba61da88115c5aa3b1e09
ramadoni16 <donicyberit@gmail.com>
2024-07-21 20:15:40 +0700

    Perulangan

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
-const shoppingCart = [
-  {
-    product: "vue.js",
-    price: 1.22,
-    quantity: 3,
-  },
-  {
-    product: "react.js",
-    price: 2.32,
-    quantity: 1,
-  },
-  {
-    product: "angular.js",
-    price: 3.32,
-    quantity: 2,
-  },
-];
-console.log(shoppingCart[0].price);
+for (let i = 1; i <= 10; i++) {
+  console.log("saya ada di dalam loop");
+  console.log(i);
+}
