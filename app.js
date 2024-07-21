754a88161c45f24356b2f15163bc2525e1bd10f3
ramadoni16 <donicyberit@gmail.com>
2024-07-21 16:02:23 +0700

    Method Array Yang Sering Digunakan

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-// Method yang dimiliki array
+//Method yang dimiliki array
+// concat - menggabungkan array
+const array1 = ["a", "b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+console.log(array3);
 
-// push menambahkan item pada akhir array
-// pop menghapus item dari paling akhir array
-// shift menghapus item dari paling awal array
-// unshift menambahkan item pada awal array
+// includes mencari suatu nilai di dalam array
+const hewan = [1, 2, 3];
+console.log(hewan.includes(2));
+// expected output: true
+const pets = ["cat", "dog", "bat"];
+console.log(pets.includes("cat"));
+// expected output: true
+console.log(pets.includes("at"));
+// expected output: false
 
-let student = ["rama", "agus", "rahman", "surya"];
-student.unshift("Herbalife");
-student.unshift("puc");
-student.unshift("jne");
-student.shift();
-student[2] = "lampung";
-console.log(student);
+// indexOf sama kaya string.indexOf
+const nama = ["ori", "manggo", "mandarin", "nrg", "line 1000"];
+console.log(nama.indexOf("ori"));
+// expected output: 1
+// start from index 2
+console.log(nama.indexOf("mandarin"));
+// expected output: 4
+console.log(nama.indexOf("multivitamin"));
+// expected output: 1
+
+// join membuat suatu string dari sebuah array
+// reverse membalikkan urutan nilai dari array
+// slice menyalin sebagian nilai dari array
+// splice menghapus atau mengubah element
+// sort - mengurutkan nilai pada array
+const bulan = ["March", "Jan", "Feb", "Dec"];
+bulan.sort();
+console.log(bulan);
+// expected output: Array ["Dec", "Feb", "Jan", "March"]
+const angka = [1, 30, 4, 21, 100000];
+angka.sort();
+console.log(angka);
+// expected output: Array [1, 100000, 21, 30, 4]
