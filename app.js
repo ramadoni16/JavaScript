3f6cb97bd431a66cfa2933feae6fcfb1a188fdb3
ramadoni16 <donicyberit@gmail.com>
2024-07-21 07:04:51 +0700

    else

--- app.js
+++ app.js
@@ -1,40 +1,10 @@
-// alert(1 + 1);
-// console.log("hello world!!!");
-
-// if (1 + 1 === 2) {
-//   console.log("di dalam pembanding");
-// }
-// console.log("setelah pembanding");
-
-// // IF
-// let angka = Math.random();
-// console.log(angka);
-
-// if (angka >= 0.5) {
-//   console.log("Angka Lebih dari 0.5");
-// }
-// if (angka <= 0.5) {
-//   console.log("Angka Lebih kecil dari 0.5");
-// }
-
-const iniHari = "sabtu";
+// const iniHari = "rabu";
+const iniHari = prompt("isikan hari").toLocaleLowerCase();
 
 if (iniHari === "senin") {
   console.log("selamat bekerja kawan!");
 } else if (iniHari === "sabtu") {
   console.log("semoga jangan cepat berlalu!");
-}
-
-const nilai = "40";
-
-if (nilai < 50) {
-  console.log("E");
-} else if (nilai < 69) {
-  console.log("D");
-} else if (nilai < 70) {
-  console.log("C");
-} else if (nilai < 80) {
-  console.log("B");
-} else if (nilai < 100) {
-  console.log("A");
+} else {
+  console.log("Yah biasa saja");
 }
