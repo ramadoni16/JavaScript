dcacfb7d46d0264745454d47ceada79a30c53f7a
ramadoni16 <donicyberit@gmail.com>
2024-07-21 07:31:25 +0700

    Operator Logic And

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-const password = prompt("buat password");
+// const password = prompt("buat password");
 
 // password harus sepanjang 6 karakter
-if (password.length >= 6) {
-  if (password.indexOf(" ") === -1) {
-    console.log("password valid");
-  } else {
-    console.log("password tidak boleh ada spasi ");
-  }
+// if (password.length >= 6) {
+//   if (password.indexOf(" ") === -1) {
+//     console.log("password valid");
+//   } else {
+//     console.log("password tidak boleh ada spasi ");
+//   }
+// } else {
+//   console.log("password minimal 6 karakter");
+// }
+
+const password = prompt("buat password");
+
+if (password.length >= 6 && password.indexOf(" ") == -1) {
+  console.log("password valid");
 } else {
-  console.log("password minimal 6 karakter");
+  console.log("password tidak memenuhi syarat");
 }
+
+// let age = 19;
+// let gender = "male";
+// if (gender === "male" && age === 19) {
+//   console.log("boleh menikah");
+// }
