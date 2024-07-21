8eabc2de1ede1f2047014b6ebde901e351bb4b8f
ramadoni16 <donicyberit@gmail.com>
2024-07-21 14:30:52 +0700

    Mendapatkan Nilai Dengan Index Dan Mengubah Isinya

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let playlist = ["fiersa", "ungu", "noah"];
-console.log(playlist[2]);
+// let playlist = ["fiersa", "ungu", "noah"];
+// console.log(playlist[2]);
+
+let warna = ["hijau", "kuning", "kelab", "bu"];
+// console.log(warna);
+warna[0] = "merah";
+warna[2] = "hijau";
+warna[3] = "biru";
+warna[8] = "ungu";
+console.log(warna);
