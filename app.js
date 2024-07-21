145e382e9926d46360d2872732ec552fb3df4702
ramadoni16 <donicyberit@gmail.com>
2024-07-21 13:54:38 +0700

    Switch Sebagai Alternatif If Statement

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
-// const password = prompt("buat password");
-
-// password harus sepanjang 6 karakter
-// if (password.length >= 6) {
-//   if (password.indexOf(" ") === -1) {
-//     console.log("password valid");
-//   } else {
-//     console.log("password tidak boleh ada spasi ");
-//   }
-// } else {
-//   console.log("password minimal 6 karakter");
-// }
-
-// const password = prompt("buat password");
-
-// if (password.length >= 6 && password.indexOf(" ") == -1) {
-//   console.log("password valid");
-// } else {
-//   console.log("password tidak memenuhi syarat");
-// }
-// Operator Logic And
-
-// let age = 19;
-// let gender = "male";
-// if (gender === "male" && age === 19) {
-//   console.log("boleh menikah");
-// }
-
-const role = prompt("masukka role akun");
-
-if (role !== "admin" || role === "spv") {
-  console.log("akses di tolak");
-} else {
-  console.log("boleh mengaksesnyaa");
+const balonku = prompt("input warna balon");
+switch (balonku) {
+  case "merah":
+    console.log("warna merah");
+    break;
+  case "hijau":
+    console.log("dooor");
+    break;
+  default:
+    console.log("bukan balon saya");
 }
