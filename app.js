70765b9ea19358cfa8abf817529d832660c1e494
ramadoni16 <donicyberit@gmail.com>
2024-07-27 21:23:35 +0700

    Perulangan Menggunakan Perintah While

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-const studentRow = [
-  ["Olivia", "Liam", "Emma", "Noah"],
-  ["Amelia", "Oliver", "Ava", "Elijah"],
-  ["Sophia", "Mateo", "Isabella", "Lucas"],
-];
-
-for (let i = 0; i < studentRow.length; i++) {
-  const row = studentRow[i];
-  console.log(`Seat Row #${i + 1}`);
-
-  for (let j = 0; j < row.length; j++) {
-    // console.log(row[j]);
-    console.log(`   ${row[j]}`);
-  }
-}
-
-// const pilihan = "abcd";
-// for (let i = 1; i <= 10; i++) {
-//   console.log(`${i}.Soal nomor ${i}:`);
-//   for (let j = 0; j < pilihan.length; j++) {
-//     console.log(`     ${pilihan[j]}. Pilihan Jawaban`);
-//   }
+// let num = 0;
+// while (num < 10) {
+//   console.log(num);
+//   num++;
 // }
+
+const Password = "Pass123";
+
+let guess = prompt("enter the password");
+while (guess !== Password) {
+  guess = prompt("enter the password");
+}
+alert("congrats! the password is correct");
