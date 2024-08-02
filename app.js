a8359c74fa88d67e52c112d3cc8cb50f4ded6d12
ramadoni16 <donicyberit@gmail.com>
2024-08-02 22:03:13 +0700

    Game Tebak Tebakkan Angka Dengan While

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-// let input = prompt("Hei, say something");
-// while (true) {
-//   input = prompt(input);
-//   if (input.toLocaleLowerCase() === "stop") break;
-// }
-// alert("ok");
-
-for (let i = 0; i < 10000; i++) {
-  console.log(i);
-  if (i === 100) break;
+let maximum = parseInt(prompt("Masukkan nilai maksimal!"));
+while (!maximum) {
+  maximum = parseInt(prompt("Masukkan nilai Maksimal!"));
 }
+
+const targetNum = Math.floor(Math.random() * maximum) + 1;
+console.log(targetNum);
+
+let guess = parseInt(prompt("Isi dengan Tebakan pertama Kamu"));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+  attempts++;
+  if (guess > targetNum) {
+    guess = prompt("Terlalu tinggi! tebak lagi:");
+  } else {
+    guess = prompt("Terlalu Rendah! Tebak Lagi");
+  }
+}
+
+alert(`Selamat Tebakan Anda Benar ! Dengan percobaan ${attempts} kali`);
