a3cdbd098f1cd2bf7967a4fe0a2d2d48afe0b94b
ramadoni16 <donicyberit@gmail.com>
2024-08-02 22:50:43 +0700

    Mendefinisikan Dan Menjalankan Function

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-// salah satu blok penyusun fundamental dalam JavaScript (FUNCTION)
-
-// let lemparDadu = Math.floor(Math.random() * 6) + 1;
-// console.log(lemparDadu);
-
-// let lemparDadu = console.log(lemparDadu);
-
-function lemparDadu() {
-  console.log(Math.floor(Math.random() * 6) + 1);
-  //   console.log(lemparDadu());
+function nyanyi() {
+  console.log("cek");
+  console.log("DO RE MI...");
 }
+nyanyi();
+nyanyi();
