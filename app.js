c8166c281e0e19e2cbb068290752abbc3e62469d
ramadoni16 <donicyberit@gmail.com>
2024-08-08 13:27:31 +0700

    Statement Return Pada Function

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// function selamatPagi(name) {
-//   console.log(`Halo, ${name} Selamat Pagi`);
-// }
+// const hasil = jumlahkan(1, 2);
 
-// selamatPagi("Rama");
+// function jumlahkan(a, b) {
+//   const total = a + b;
+//   return total;
+// }
+// console.log(jumlahkan(5, 8));
 
 function jumlahkan(a, b) {
-  const total = a + b;
-  console.log(total);
+  if (typeof a !== "number" || typeof b !== "number") {
+    // return false;
+    return "bukan angka";
+  }
+  return a + b;
 }
-jumlahkan(1, 2);
+console.log(jumlahkan("a", "b"));
+console.log(jumlahkan(3, 4));
