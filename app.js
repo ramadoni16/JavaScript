9cc6ddd314c577fa3e07aa67206154a81a44733c
ramadoni16 <donicyberit@gmail.com>
2024-08-08 13:12:07 +0700

    Multiple Parameter Dan Argument

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function selamatPagi(name) {
-  console.log(`Halo, ${name} Selamat Pagi`);
-}
+// function selamatPagi(name) {
+//   console.log(`Halo, ${name} Selamat Pagi`);
+// }
 
-selamatPagi("Rama");
+// selamatPagi("Rama");
+
+function jumlahkan(a, b) {
+  const total = a + b;
+  console.log(total);
+}
+jumlahkan(1, 2);
